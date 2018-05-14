cd2e6b549967eae9c997933c475587470238f7df
lywworking <xue_2013@sina.com>
2018-05-14 16:12:24 +0800

    Update CommandShellRunner.java

--- src/main/java/org/poem/core/CommandShellRunner.java
+++ src/main/java/org/poem/core/CommandShellRunner.java
@@ -1,10 +1,7 @@
 package org.poem.core;
 
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.*;
 import org.poem.api.Runner;
 import org.poem.config.ApplicationContext;
 import org.poem.config.ShellCommandParse;
@@ -18,12 +15,18 @@ import org.poem.tools.utils.collection.Lists;
 import org.poem.tools.utils.logger.LoggerUtils;
 import org.poem.tools.utils.string.StringUtils;
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
 
 import java.io.PrintWriter;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Executable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 执行器
@@ -55,19 +58,14 @@ public class CommandShellRunner implements Runner {
             pw.flush();
             String commandLine = sc.nextLine();  //读取字符串型输入
             if (StringUtils.isNotBlank(commandLine)) {
-                String[] args = commandLine.split("\\s+");
-                String method = args[0];
-                List<ShellMethodTarget> shellMethodTargets = Collecommands.values();
-                if (ActionEnums.HELP.equals(method)) {
-                    shellMethodTargets = Collections.addAll(commands.values());
-                }
-                ShellCommandParse parse = new ShellCommandParse(commands.getOrDefault(method, Lists.empty()));
+                ShellCommandParse parse = new ShellCommandParse(commands.get(getGroupName(commandLine)));
                 try {
-                    CommandLine line = parser.parse(parse.createParse().getSearchOpts(), args);
-                    String[] as = line.getArgs();
+                    Object[] args = parse.getParameterValue(commandLine);
+                    String beanName = getGroupName(commandLine);
+                    String method = getCommand(commandLine);
+                    executor(beanName,method,Arrays.asList(args),parse.getParameterTypes());
                 } catch (ParseException e) {
-                    ShellPrint.printMsg(e.getMessage());
-                    ShellPrint.printUnrecognizOption(method, args[1], parse.createParse().getSearchOpts());
+                    e.printStackTrace();
                 }
             } else {
                 System.err.println("\n");
@@ -77,42 +75,68 @@ public class CommandShellRunner implements Runner {
 
 
     /**
+     * 获取参数
+     *
+     * @param commandLine
+     * @return
+     */
+    private String getCommand(String commandLine) {
+        if (StringUtils.isNoneBlank(commandLine)) {
+            List<String> cpmmands = Arrays.asList(commandLine.split("\\s+"));
+            if (cpmmands.size() >= 2) {
+                return cpmmands.get(1);
+            }
+        }
+        return "";
+    }
+
+    /**
+     * 获取当前的分组
+     *
+     * @param commandLine
+     * @return
+     */
+    private String getGroupName(String commandLine) {
+        if (StringUtils.isNoneBlank(commandLine)) {
+            List<String> cpmmands = Arrays.asList(commandLine.split("\\s+"));
+            if (cpmmands.size() >= 1) {
+                return cpmmands.get(0);
+            }
+        }
+        return "";
+    }
+
+    /**
      * 执行器
      *
      * @param bean       bean名字
      * @param methodName 方法
      * @param parameters 参数
      */
-    private void executor(String bean, String methodName, List<Object> parameters) {
+    private void executor(String bean, String methodName, List<Object> parameters, Class<?> ... parameterTypes) {
         try {
             if (!org.springframework.util.StringUtils.isEmpty(bean)) {
                 Object clsObj = ApplicationContext.getBean(bean);
-                if (null == clsObj) {
-                    LoggerUtils.info("任务:" + bean + "指定的类未被Spring找到!");
-                    return;
-                }
                 SObject result;
                 Class clazz = clsObj.getClass();
                 //得到指定的方法
-                Method method = clazz.getDeclaredMethod(methodName);
+                Method method = clazz.getMethod(methodName,parameterTypes);
                 if (null == method) {
                     LoggerUtils.info("任务:" + methodName + "指定的方法未被Spring找到!");
                     return;
                 }
-                if (CollectionUtils.isNotEmpty(parameters)) {
-                    //执行方法
-                    result = (SObject) method.invoke(clsObj, parameters);
-                } else {
-                    //执行方法
-                    result = (SObject) method.invoke(clsObj);
+                String name = method.getReturnType().getSimpleName();
+                if(!"void".equals(name)){
+                    result = (SObject) ReflectionUtils.invokeMethod(method, clsObj, parameters);
+                    ShellPrint.printResult(result);
                 }
-                ShellPrint.printResult(result);
             }
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalArgumentException | IllegalAccessException e) {
+        } catch (NoSuchMethodException | IllegalArgumentException e) {
+            e.printStackTrace();
             ShellPrint.printMsg(e.getMessage());
         } finally {
             //打印数据
-            ShellPrint.printMsg("/n");
+            ShellPrint.printMsg("\n");
         }
     }
 }
